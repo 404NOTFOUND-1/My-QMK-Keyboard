9f45022e00cb900c895ec1a00b91339f4ca0ea31
404NOTFOUND <lzw1477@163.com>
2025-04-12 13:30:34 +0800

    update

--- a11/config.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-/* key matrix size */
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 16
-
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCE 4
--- a11/keymaps/vial/config.h
+++
@@ -1,6 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-
-#pragma once
-
-#define VIAL_KEYBOARD_UID {0xA4, 0xD8, 0x7A, 0xC7, 0xCA, 0xD2, 0xC0, 0x35}
-
